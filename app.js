3c9d81ff5b74aa7606102edb9a5dacbbb1cf534b
daisykumar <56020145+daisykumar@users.noreply.github.com>
2021-02-05 18:37:06 +0100

    update

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
-
 
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
@@ -28,14 +22,7 @@ function topFunction() {
 }
 
 navToggle.addEventListener("click", function (){
-    //console.log(links.classList);
-    //console.log(links.classList.contains("random"));
-    //console.log(links.classList.contains("links"));
-    //if(links.classList.contains("show-links")){
-    //     links.classList.remove("show-links") 
-    //} else {
-    //   links.classList.add("show-links");
-    //}
+
     links.classList.toggle("show-links");
     links.classList.toggle("show-socials");
 
